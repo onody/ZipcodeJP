d2e6c1a3435f4a666bead70870475cf2c7bcb1df
Kohei Onodera <onodera212@gmail.com>
2014-08-17 14:14:15 +0900

    Initial Commit

---
+++ ZipcodeJP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZipcodeJP
+//
+//  Created by 小野寺耕平 on 2014/08/17.
+//  Copyright (c) 2014年 Kohei Onodera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
